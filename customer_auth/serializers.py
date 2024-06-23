2c26eee2cfd3f3339312426aaa7805a1d0ceb9f5
Gaurav Bhinda <gaurav.bhinda@e2enetworks.com>
2024-06-24 00:09:14 +0530

    adding check

--- customer_auth/serializers.py
+++ customer_auth/serializers.py
@@ -12,11 +12,17 @@ class CustomUserSerializer(serializers.Serializer):
 
     def validate(self, obj):
         email_already_exists = Customer.objects.filter(email=obj.get('email'))
+        phone_number_exists = Customer.objects.filter(phone_number=obj.get('phone_number'))
         if email_already_exists:
             raise serializers.ValidationError("Email already taken")
         
+        if phone_number_exists:
+            raise serializers.ValidationError("Phone number already exists")
+        
         if obj.get('password') != obj.get('confirm_password'):
             raise serializers.ValidationError("The passwords do not match")
+        
+
         
         return obj
         
