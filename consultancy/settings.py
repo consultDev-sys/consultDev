ee2de3e60ec299d590bb1e4b7858cbdd151c6b47
Gaurav Bhinda <gaurav.bhinda@e2enetworks.com>
2024-06-24 22:21:59 +0530

    production one

--- consultancy/settings.py
+++ consultancy/settings.py
@@ -29,13 +29,14 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','164.52.203.34', '164.52.203.34.nip.io']
+ALLOWED_HOSTS = ['127.0.0.1','164.52.203.34', '164.52.203.34.nip.io', '52.91.67.106']
 
 CORS_ALLOW_ALL_ORIGINS = True
 
 CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1',
     'http://164.52.203.34/',
+    'http://52.91.67.106',
     'https://164.52.203.34.nip.io'
 ]
 
